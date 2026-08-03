8635bb99ddb41516c92d84e0a46c13dfdcd3ae53
User <user@example.com>
2026-08-03 16:52:00 +0300

    Reduce client carousel speed to 2000ms

--- components/software-company-components/FeaturePartners/index.js
+++ components/software-company-components/FeaturePartners/index.js
@@ -25,7 +25,7 @@ const partners = [...basePartners, ...basePartners]
 var settings = {
   dots: false,
   infinite: true,
-  speed: 600,
+  speed: 2000,
   slidesToShow: 7,
   slidesToScroll: 1,
   autoplay: true,
