4e267272046bbedafe519b6dbcf7d10eba449717
User <user@example.com>
2026-08-04 14:35:23 +0300

    update TechnologySection heading and logo path to partners folder

--- components/software-company-components/TechnologySection/TechnologySection.js
+++ components/software-company-components/TechnologySection/TechnologySection.js
@@ -4,23 +4,23 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 
-import logo1 from 'public/images/clients/client_logo_1.webp';
-import logo2 from 'public/images/clients/client_logo_2.webp';
-import logo3 from 'public/images/clients/client_logo_3.webp';
-import logo4 from 'public/images/clients/client_logo_4.webp';
-import logo5 from 'public/images/clients/client_logo_5.webp';
-import logo6 from 'public/images/clients/client_logo_6.webp';
-import logo7 from 'public/images/clients/client_logo_7.webp';
+import logo1 from 'public/images/partners/partner_logo_1.webp';
+import logo2 from 'public/images/partners/partner_logo_2.webp';
+import logo3 from 'public/images/partners/partner_logo_3.webp';
+import logo4 from 'public/images/partners/partner_logo_4.webp';
+import logo5 from 'public/images/partners/partner_logo_5.webp';
+import logo6 from 'public/images/partners/partner_logo_6.webp';
+import logo7 from 'public/images/partners/partner_logo_7.webp';
 
 const brandLogos = [
-    { id: 'logo-1', image: logo1 },
-    { id: 'logo-2', image: logo2 },
-    { id: 'logo-3', image: logo3 },
-    { id: 'logo-4', image: logo4 },
-    { id: 'logo-5', image: logo5 },
-    { id: 'logo-6', image: logo6 },
-    { id: 'logo-7', image: logo7 },
-    { id: 'logo-8', image: logo2 },
+{ id: 'logo-1', image: logo1 },
+{ id: 'logo-2', image: logo2 },
+{ id: 'logo-3', image: logo3 },
+{ id: 'logo-4', image: logo4 },
+{ id: 'logo-5', image: logo5 },
+{ id: 'logo-6', image: logo6 },
+{ id: 'logo-7', image: logo7 },
+{ id: 'logo-8', image: logo1 },
 ];
 
 const sliderSettings = {
@@ -65,11 +65,11 @@ const TechnologySection = () => (
     <div className="section_space">
         <div className="heading_block text-center">
             <div
-                className="heading_focus_text has_underline d-inline-flex"
-                style={{ backgroundImage: `url(${'/images/shapes/shape_title_under_line.svg'})` }}
-            >
-                Brand We Work With
-            </div>
+className="heading_focus_text has_underline d-inline-flex"
+style={{ backgroundImage: `url(${'/images/shapes/shape_title_under_line.svg'})` }}
+>
+Brands We Work With
+</div>
             <h2 className="heading_text mb-0">
                 Trusted <mark>Technology Partners</mark>
             </h2>
